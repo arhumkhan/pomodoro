9f636b04361917874f52b8955437371d70df5d08
Arhum Khan <37052066+arhumkhan@users.noreply.github.com>
2019-02-21 10:45:38 -0500

    Added class constructor
    Basic contsructor and method, kind of a working draft

--- pomodoro.rb
+++ pomodoro.rb
@@ -1,3 +1,18 @@
 class Pomodoro
+  attr_reader :work
+  def initialize(work=25, short_break=5, long_break=15)
+  	@work = work
+  	@short_break = short_break
+  	@long_break = long_break
+  end
 
+  def run_work_period
+    puts "Begin work for #{@work} seconds."
+    @work.times {
+  	  sleep 1
+  	}
+  end
 end
+
+pomo = Pomodoro.new
+pomo.run_work_period
