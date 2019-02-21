cc525f591cd76e374c0747173c6856273d0097a2
Arhum Khan <37052066+arhumkhan@users.noreply.github.com>
2019-02-21 13:04:15 -0500

    Redefined constructor and added methods
    Added more attributes to the constructor, made more specfic methods

--- pomodoro.rb
+++ pomodoro.rb
@@ -1,18 +1,56 @@
 class Pomodoro
   attr_reader :work
+  attr_reader :total_time
   def initialize(work=25, short_break=5, long_break=15)
-  	@work = work
-  	@short_break = short_break
-  	@long_break = long_break
+  	@work = work * 60
+  	@short_break = short_break * 60
+  	@long_break = long_break * 60
+  	@total_time = 0 #in seconds
+  end
+
+  def countdown(secs)
+    time_left = Time.at(secs)
+    secs.times {
+      print "#{time_left.min} m #{time_left.sec} s left \r"
+      sleep 1
+      time_left -= 1
+      @total_time += 1
+    }
+    print "\n"
   end
 
   def run_work_period
-    puts "Begin work for #{@work} seconds."
-    @work.times {
-  	  sleep 1
-  	}
+  	puts "Begin work for #{@work / 60} minutes."
+  	countdown(@work)
+  end
+
+  def run_short_break
+  	puts "Break for #{@short_break / 60} minutes."
+  	countdown(@short_break)
+  end
+
+  def run_long_break
+  	puts "Enjoy a long break for #{@long_break / 60} minutes."
+  	countdown(@long_break)
+  end
+
+  def run_set
+  	3.times {
+      run_work_period
+      run_short_break
+    }
+    run_work_period
+  	run_long_break
   end
 end
 
-pomo = Pomodoro.new
-pomo.run_work_period
+pomo = Pomodoro.new(1,0,0)
+
+input = "y"
+until input == "n"
+  pomo.run_set
+  puts "Run another set of #{pomo.work / 60} minute pomodoros? (y/n)"
+  input = gets.chomp.downcase
+end
+
+puts "Total time: #{pomo.total_time / 60} minutes."
